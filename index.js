fdd47cb346c48d304f1430081501a0692a71217e
Tarcisio <tarcisio.angelo@gmail.com>
2021-02-15 11:08:07 -0300

    Separando as coisas

--- index.js
+++ index.js
@@ -1,66 +1,61 @@
-require("dotenv-safe").config();
-const jwt = require('jsonwebtoken');
-const express = require('express');
-const bodyParser = require('body-parser');
-const http = require('http');
-const app = express();
-const db = require('./db');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+require("dotenv-safe").config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const http = require('http')
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Todas as rotas tem que ficar nesse padrão
+const routes = require('./routes')
+app.use(routes)
 
 
-const order = [];
+// Muda o resto ai
 
-app.post('/login', (req, res, next) => {
-    if (req.body.user === 'admin@test.com' && req.body.password === 'G5e2l5d7#') {
-        const id = 1; //vem do banco
-        const token = jwt.sign({ id }, process.env.SECRET, {
-            expiresIn: 30000
-        });
-        return res.status(200).json({ auth: true, token: token });
-    }
-    res.status(500).json({ message: 'Login inválido!' });
-});
+
+
+
+// Middlewares
+const verifyJWT = require('./middlewares/auth')
 
 app.get('/order', verifyJWT, (req, res) => {
-    return res.json(order);
-});
+    const { userID } = req
+
+    const orders = []
+
+    console.log('ID do usuário que veio do token: ', userID)
+    
+    return res.json({ orders })
+})
 
 app.get('/order/:index', (req, res) => {
-    return res.json('dasas');
-});
+    return res.json('dasas')
+})
 
 app.post('/order', (req, res) => {
-    const { no_product, id_user, id_product, id_adress, id_payment } = req.body;
-    order.push(no_product, id_user, id_product, id_adress, id_payment);
+    const { no_product, id_user, id_product, id_adress, id_payment } = req.body
 
-    return res.json(order);
-});
+    const orders = []
+
+    orders.push(no_product, id_user, id_product, id_adress, id_payment)
+
+    return res.json({ orders })
+})
 
 app.put('/order/:index', (req, res) => {
-    const { id_product } = req.params;
-    return res.json("Altera pedidos");
+    const { id_product } = req.params
+    return res.json("Altera pedidos")
 
-});
+})
 
 app.delete('/order', (req, res) => {
     return res.json({
         message: 'Exclui Pedidos'
-    });
+    })
+})
 
-});
 
-function verifyJWT(req, res, next) {
-    const token = req.headers['authorization'];
-    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+const server = http.createServer(app)
 
-    jwt.verify(token, process.env.SECRET, function (err, decoded) {
-        if (err) return res.status(500).json({ auth: false, message: 'Falha na autenticação.' });
-        req.userId = decoded.id;
-        next();
-    });
-}
-
-const server = http.createServer(app);
-server.listen(3000);
-console.log("Server on-line");
+server.listen(4000, () => console.log("Server on-line"))
