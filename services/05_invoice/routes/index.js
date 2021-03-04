5597376ab861f09768bf85fa5ee4d8aa2682e5fb
Tarcisio <tarcisio.angelo@gmail.com>
2021-03-04 13:26:15 -0300

    Removendo rotas do invoice

--- services/05_invoice/routes/index.js
+++ services/05_invoice/routes/index.js
@@ -1,13 +1,8 @@
 const express = require('express')
 const routes = express.Router()
-const queue = require('../rabbitmq')
 const EmptyToNull = require('../middlewares/EmptyToNull')
 
 // Middleware Geral
 routes.use(EmptyToNull)
 
-routes.post('/email', (req, res) => {
-    queue.sendToQueue("email", req.body)
-    res.json({ message: 'e-mail enviado' })
-})
 module.exports = routes
